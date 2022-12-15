47e70a579c50b0a40507bde6407c344732215f4d
Safayet Mahid <safayetmahid96@gmail.com>
2022-12-16 01:12:09 +0600

    current user and protected route for checkout implemented

---
+++ src/routers/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import useAuth from "../custom-hooks/useAuth";
+
+const ProtectedRoute = ({ children }) => {
+  const { currentUser } = useAuth();
+  //   console.log(currentUser);
+
+  return currentUser ? children : <Navigate to="login" replace={true} />;
+};
+
+export default ProtectedRoute;
--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -1,31 +1,36 @@
+import React from "react";
 
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import React from 'react'
-
-import {Routes,Route,Navigate} from "react-router-dom"
-
-
-import Home from '../pages/Home'
-import Shop from '../pages/Shop'
-import Cart from '../pages/Cart'
-import ProductDetails from '../pages/ProductDetails'
-import Checkout from '../pages/Checkout'
-import Login from '../pages/Login'
-import Signup from '../pages/Signup'
+import Home from "../pages/Home";
+import Shop from "../pages/Shop";
+import Cart from "../pages/Cart";
+import ProductDetails from "../pages/ProductDetails";
+import Checkout from "../pages/Checkout";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import ProtectedRoute from "./ProtectedRoute";
 
 const Routers = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navigate to="home"/>}/>
-      <Route path='home' element={<Home/>}/>
-      <Route path='shop' element={<Shop/>}/>
-      <Route path='shop/:id' element={<ProductDetails/>}/>
-      <Route path='cart' element={<Cart/>}/>
-      <Route path='checkout' element={<Checkout/>}/>
-      <Route path='login' element={<Login/>}/>
-      <Route path='signup' element={<Signup/>}/>
+      <Route path="/" element={<Navigate to="home" />} />
+      <Route path="home" element={<Home />} />
+      <Route path="shop" element={<Shop />} />
+      <Route path="shop/:id" element={<ProductDetails />} />
+      <Route path="cart" element={<Cart />} />
+      <Route
+        path="checkout"
+        element={
+          <ProtectedRoute>
+            <Checkout />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default Routers
+export default Routers;
