b04e91bbcb38cb2a01caa85478b92689c220c9ca
Safayet Mahid <safayetmahid96@gmail.com>
2022-12-15 19:04:32 +0600

    checkout page is done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -58,13 +58,14 @@ const Cart = () => {
               taxes and shipping will calculate in checkout
             </p>
             <div>
-              <button className="buy__btn w-100 ">
-                <Link to="/checkout">Checkout</Link>
-              </button>
-
-              <button className="buy__btn w-100 mt-3 mb-3">
-                <Link to="/shop">Continue Shopping</Link>
-              </button>
+              <Link to="/checkout">
+                <button className="buy__btn w-100 ">Checkout</button>
+              </Link>
+              <Link to="/shop">
+                <button className="buy__btn w-100 mt-3 mb-3">
+                  Continue Shopping
+                </button>
+              </Link>
             </div>
           </Col>
         </Row>
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,11 +1,82 @@
+import React from "react";
+import Helmet from "../components/helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
 
+import { Container, Row, Col, Form, FormGroup } from "reactstrap";
 
-import React from 'react'
+import "../styles/checkout.css";
+import { useSelector } from "react-redux";
 
 const Checkout = () => {
-  return (
-    <div>Checkout</div>
-  )
-}
+  const totalQty = useSelector((state) => state.cart.totalQuantity);
+  const totalAmount = useSelector((state) => state.cart.totalAmount);
 
-export default Checkout
+  return (
+    <Helmet title="Checkout">
+      <CommonSection title="Checkout" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="8">
+              <h6 className="mb-4 fw-bold">Billing Information</h6>
+              <Form className="billing__form">
+                <FormGroup className="form__group">
+                  <input type="text" placeholder="Enter your name" />
+                </FormGroup>
+
+                <FormGroup className="form__group">
+                  <input type="email" placeholder="Enter your email" />
+                </FormGroup>
+
+                <FormGroup className="form__group">
+                  <input type="number" placeholder="Phone number" />
+                </FormGroup>
+
+                <FormGroup className="form__group">
+                  <input type="text" placeholder="Street address" />
+                </FormGroup>
+
+                <FormGroup className="form__group">
+                  <input type="text" placeholder="City" />
+                </FormGroup>
+
+                <FormGroup className="form__group">
+                  <input type="text" placeholder="Postal code" />
+                </FormGroup>
+
+                <FormGroup className="form__group">
+                  <input type="text" placeholder="Country" />
+                </FormGroup>
+              </Form>
+            </Col>
+
+            <Col lg="4">
+              <div className="checkout__cart">
+                <h6>
+                  Total Qty : <span>{totalQty}</span>
+                </h6>
+                <h6>
+                  Subtotal : <span>${totalAmount}</span>
+                </h6>
+                <h6>
+                  <span>
+                    Shipping : <br /> free shipping
+                  </span>
+                  <span>$0</span>
+                </h6>
+                <h4>
+                  Total cost : <span>${totalAmount}</span>
+                </h4>
+                <button className="buy__btn auth__btn w-100">
+                  Place an order
+                </button>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Checkout;
