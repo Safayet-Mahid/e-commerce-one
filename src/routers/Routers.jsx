8386763ce7d1068b0bde54827e96e68e16d9f450
Safayet Mahid <safayetmahid96@gmail.com>
2022-12-20 11:50:03 +0600

    router path / added

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -19,22 +19,22 @@ import Users from "../admin/Users";
 const Routers = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/home" />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/shop" element={<Shop />} />
-      <Route path="/shop/:id" element={<ProductDetails />} />
-      <Route path="/cart" element={<Cart />} />
+      <Route path="/" element={<Navigate to="home" />} />
+      <Route path="home" element={<Home />} />
+      <Route path="shop" element={<Shop />} />
+      <Route path="shop/:id" element={<ProductDetails />} />
+      <Route path="cart" element={<Cart />} />
 
       <Route path="/*" element={<ProtectedRoute />}>
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard/all-products" element={<AllProducts />} />
-        <Route path="/dashboard/add-products" element={<AddProducts />} />
-        <Route path="/dashboard/users" element={<Users />} />
+        <Route path="checkout" element={<Checkout />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="dashboard/all-products" element={<AllProducts />} />
+        <Route path="dashboard/add-products" element={<AddProducts />} />
+        <Route path="dashboard/users" element={<Users />} />
       </Route>
 
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<Signup />} />
     </Routes>
   );
 };
