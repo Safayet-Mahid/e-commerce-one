75b2a76e4591a03bd0501cdf540db37ff39e820e
Safayet Mahid <safayetmahid96@gmail.com>
2022-12-20 12:16:12 +0600

    footer placement correction

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,19 +4,19 @@ import Header from "../header/Header";
 import Routers from "../../routers/Routers";
 import { useLocation } from "react-router-dom";
 import AdminNav from "../../admin/AdminNav";
-
+import "../../styles/layout.css";
 const Layout = () => {
   const location = useLocation();
 
   return (
-    <>
+    <div>
       {location.pathname.startsWith("/dashboard") ? <AdminNav /> : <Header />}
 
-      <div>
+      <div className="layout">
         <Routers />
       </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
